df356cf83f2b8e74bfe0e61b7ee3c3a32a1f889d
Felix P <felix.pescarmona@saucelabs.com>
2020-10-26 16:01:51 -0700

    Custom stream to write sync on disk

--- src/console-wrapper.js
+++ src/console-wrapper.js
@@ -3,19 +3,21 @@
 const { HOME_DIR } = require('./constants');
 const fs = require('fs');
 const path = require('path');
+const stream = require('stream');
 const child_process = require('child_process');
 
 (async () => {
-    const fd = fs.createWriteStream(path.join(HOME_DIR, '/reports/testcafe.log'));
+    const fd = fs.openSync(path.join(HOME_DIR, '/reports/testcafe.log'), 'w+', 0644);
+    const ws = stream.Writable({
+        write: (data, encoding, cb) => fs.write(fd, data, undefined, encoding, cb),
+    })
+
     child = child_process.spawn('node', [path.join(HOME_DIR, './src/testcafe-runner.js')]);
 
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
-    child.stdout.pipe(fd);
-    child.stderr.pipe(fd);
+    child.stdout.pipe(ws);
+    child.stderr.pipe(ws);
 
-    await new Promise((acc) => {
-        fd.close();
-        child.on('exit', () => acc());
-    });
+    child.on('exit', () => fs.closeSync(fd));
 })();
