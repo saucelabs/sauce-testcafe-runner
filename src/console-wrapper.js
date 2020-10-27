c917fe8efb87fc43cb5dc3275e994dac8a8ad9cb
Félix P <felix.pescarmona@saucelabs.com>
2020-10-27 11:01:09 -0700

    feat: Export console output to SauceLabs (#19)
    * Record testcafe console
    * Upload console to SauceLabs if there is credentials available

---
+++ src/console-wrapper.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { HOME_DIR } = require('./constants');
+const fs = require('fs');
+const path = require('path');
+const stream = require('stream');
+const child_process = require('child_process');
+
+(async () => {
+    const fd = fs.openSync(path.join(HOME_DIR, 'reports', 'console.log'), 'w+', 0o644);
+    const ws = stream.Writable({
+        write (data, encoding, cb) { fs.write(fd, data, undefined, encoding, cb) },
+    });
+
+    const [nodeBin] = process.argv;
+    const child = child_process.spawn(nodeBin, [path.join(HOME_DIR, 'src', 'testcafe-runner.js')]);
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.stdout.pipe(ws);
+    child.stderr.pipe(ws);
+
+    child.on('exit', (exitCode) => ws.end(() => {
+        fs.closeSync(fd);
+        process.exit(exitCode);
+    }));
+})();
