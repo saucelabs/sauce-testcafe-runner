bc66ae39294b5ee7c4b0fa3fb2fbe8e5f59f107b
Felix P <felix.pescarmona@saucelabs.com>
2020-10-27 09:29:52 -0700

    Carry exitCode + extended use of path.join()

--- src/console-wrapper.js
+++ src/console-wrapper.js
@@ -7,17 +7,20 @@ const stream = require('stream');
 const child_process = require('child_process');
 
 (async () => {
-    const fd = fs.openSync(path.join(HOME_DIR, '/reports/console.log'), 'w+', 0644);
+    const fd = fs.openSync(path.join(HOME_DIR, 'reports', 'console.log'), 'w+', 0o644);
     const ws = stream.Writable({
-        write: (data, encoding, cb) => fs.write(fd, data, undefined, encoding, cb),
-    })
+        write (data, encoding, cb) { fs.write(fd, data, undefined, encoding, cb) },
+    });
 
-    child = child_process.spawn('node', [path.join(HOME_DIR, './src/testcafe-runner.js')]);
+    const child = child_process.spawn('node', [path.join(HOME_DIR, 'src', 'testcafe-runner.js')]);
 
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
     child.stdout.pipe(ws);
     child.stderr.pipe(ws);
 
-    child.on('exit', () => ws.end(() => fs.closeSync(fd)));
+    child.on('exit', (exitCode) => ws.end(() => {
+        fs.closeSync(fd);
+        process.exit(exitCode);
+    }));
 })();
