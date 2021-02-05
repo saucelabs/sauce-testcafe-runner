c55c6895c8ec8d7e7d0777a7388c062d5b7623b9
dpgraham <dpgraham.85@gmail.com>
2021-02-05 13:38:23 -0800

    print release id

--- .github/workflows/helpers/print-release-id.js
+++ .github/workflows/helpers/print-release-id.js
@@ -5,8 +5,8 @@ const { get } = require('https');
  * Get the release associated with a tag and print the release ID to console
  * @param {string} tag 
  */
-const printReleaseId = (repo, tag) => new Promise(async (resolve) => {
-  const apiEndpoint = `https://api.github.com/repos/saucelabs/${repo}/releases/tags/${tag}`;
+const printReleaseId = (tag) => new Promise(async (resolve) => {
+  const apiEndpoint = `https://api.github.com/repos/saucelabs/sauce-testcafe-runner/releases/tags/${tag}`;
   console.log(`Hitting endpoint: '${apiEndpoint}'`)
   get(apiEndpoint, {
     headers: {
@@ -27,13 +27,12 @@ const printReleaseId = (repo, tag) => new Promise(async (resolve) => {
 });
 
 if (require.main === module) {
-  let repo = process.env.GH_REPO;
   let ref = process.env.GH_REF;
   let [,type,tag] = ref.split('/');
   if (!['tag', 'tags'].includes(type)) {
     tag = 'v0.1.9'; // <-- for testing purposes
   }
-  printReleaseId(repo, tag)
+  printReleaseId(tag)
     .then(() => process.exit(0))
     .catch(() => process.exit(1));
 }
