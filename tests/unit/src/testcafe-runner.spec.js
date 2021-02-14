d1547e51d399f8fc467fcb4f6c931176672a0e92
Dan Graham <dan@saucelabs.com>
2021-02-14 12:34:00 -0800

    add testcafe unit tests (#40)

---
+++ tests/unit/src/testcafe-runner.spec.js
@@ -0,0 +1,188 @@
+jest.mock('testcafe');
+jest.mock('../../../src/utils');
+jest.mock('../../../src/sauce-testreporter');
+const { run } = require('../../../src/testcafe-runner');
+const utils = require('../../../src/utils');
+const { sauceReporter } = require('../../../src/sauce-testreporter');
+const testcafe = require('testcafe');
+
+const baseRunCfg = {
+  testcafe: {
+    projectPath: '../',
+    version: '1.8.5'
+  },
+};
+const baseSuite = {
+  name: 'fake-suite-name',
+  browserName: 'chrome',
+  src: ['tests/*.*'],
+  selectorTimeout: 1234,
+  skipJsErrors: true,
+  quarantineMode: true,
+  skipUncaughtErrors: true,
+  assertionTimeout: 1234,
+  pageLoadTimeout: 1234,
+  speed: 1234,
+  stopOnFirstFail: true,
+  disablePageCaching: true,
+  disableScreenshots: true,
+};
+
+describe('.run', function () {
+  let createRunner, runner, backupEnv;
+  let date = 0;
+  let runReturnValue;
+  beforeEach(function () {
+    jest.spyOn(Date.prototype, 'toISOString').mockImplementation(() => '' + date++);
+    backupEnv = process.env;
+    utils.getAbsolutePath.mockImplementation((path) => path);
+    utils.getSuite.mockImplementation((runCfg, suiteName) => (
+      runCfg.suites.find((testSuite) => testSuite.name === suiteName)
+    ));
+    testcafe.mockImplementation(() => {
+      createRunner = jest.fn(function () {
+        runner = jest.fn();
+        runner.src = jest.fn(function () { return this; });
+        runner.browsers = jest.fn(function () { return this; });
+        runner.concurrency = jest.fn(function () { return this; });
+        runner.reporter = jest.fn(function () { return this; });
+        runner.tsConfigPath = jest.fn(function () { return this; });
+        runner.clientScriptPath = jest.fn(function () { return this; });
+        runner.video = jest.fn(function () { return this; });
+        runner.screenshots = jest.fn(function () { return this; });
+        runner.run = jest.fn(() => runReturnValue || 0);
+        return runner;
+      });
+      return { createRunner };
+    });
+  });
+  afterEach(function () {
+    process.env = backupEnv;
+    sauceReporter.mockRestore();
+  });
+  it('calls TestCafe method with a kitchen sink runCfg (Docker mode)', async function () {
+    process.env = {
+      SAUCE_USERNAME: 'fake',
+      SAUCE_ACCESS_KEY: 'fake',
+      SAUCE_VM: '',
+    };
+    utils.loadRunConfig.mockImplementation(() => ({
+      ...baseRunCfg,
+      suites: [
+        {
+          ...baseSuite,
+          screenshots: {
+            'takeOnFails': true
+          },
+          clientScripts: ['fake', 'scripts'],
+          tsConfigPath: '/fake/tsconfig/path',
+        }
+      ]
+    }));
+    const passed = await run('/fake/path/to/runCfg', 'fake-suite-name');
+    expect(passed).toBe(true);
+    const results = {
+      'src': runner.src.mock.calls,
+      'browsers': runner.browsers.mock.calls,
+      'concurrency': runner.concurrency.mock.calls,
+      'reporter': runner.reporter.mock.calls,
+      'tsConfigPath': runner.tsConfigPath.mock.calls,
+      'clientScriptPath': runner.clientScriptPath.mock.calls,
+      'video': runner.video.mock.calls,
+      'screenshots': runner.screenshots.mock.calls,
+      'run': runner.run.mock.calls,
+    };
+    expect(results).toMatchSnapshot();
+    expect(sauceReporter.mock.calls).toMatchSnapshot();
+  });
+  it('calls TestCafe method with a kitchen sink runCfg (Sauce VM mode)', async function () {
+    process.env = {
+      SAUCE_USERNAME: 'fake',
+      SAUCE_ACCESS_KEY: 'fake',
+      SAUCE_VM: 'truth',
+      SAUCE_BROWSER_PATH: 'browser:/fake/browser'
+    };
+    utils.loadRunConfig.mockImplementation(() => ({
+      ...baseRunCfg,
+      suites: [
+        {
+          ...baseSuite,
+          screenshots: {
+            'takeOnFails': true
+          },
+          clientScripts: ['fake', 'scripts'],
+          tsConfigPath: '/fake/tsconfig/path',
+        }
+      ]
+    }));
+    const passed = await run('/fake/path/to/runCfg', 'fake-suite-name');
+    expect(passed).toBe(true);
+    const results = {
+      'src': runner.src.mock.calls,
+      'browsers': runner.browsers.mock.calls,
+      'concurrency': runner.concurrency.mock.calls,
+      'reporter': runner.reporter.mock.calls,
+      'tsConfigPath': runner.tsConfigPath.mock.calls,
+      'clientScriptPath': runner.clientScriptPath.mock.calls,
+      'video': runner.video.mock.calls,
+      'screenshots': runner.screenshots.mock.calls,
+      'run': runner.run.mock.calls,
+    };
+    expect(results).toMatchSnapshot();
+    expect(sauceReporter.mock.calls).toMatchSnapshot();
+  });
+  it('reports nothing if no SAUCE_USERNAME or SAUCE_ACCESS_KEY', async function () {
+    process.env = {
+      SAUCE_USERNAME: '',
+      SAUCE_ACCESS_KEY: '',
+      SAUCE_VM: '',
+    };
+    utils.loadRunConfig.mockImplementation(() => ({
+      ...baseRunCfg,
+      suites: [
+        {
+          ...baseSuite,
+          screenshots: {
+            'takeOnFails': true
+          },
+          clientScripts: ['fake', 'scripts'],
+          tsConfigPath: '/fake/tsconfig/path',
+        }
+      ]
+    }));
+    await run('/fake/path/to/runCfg', 'fake-suite-name');
+    expect(sauceReporter.mock.calls).toEqual([]);
+  });
+  it('fails if provide a fake browser', async function () {
+    utils.loadRunConfig.mockImplementation(() => ({
+      ...baseRunCfg,
+      suites: [
+        {
+          ...baseSuite,
+          browserName: 'GrahamBrowser',
+        }
+      ]
+    }));
+    process.env = {
+      SAUCE_VM: '',
+    };
+    const passed = await run('/fake/path/to/runCfg', 'fake-suite-name');
+    expect(passed).toBe(false);
+  });
+  it('fails if run returns non-zero', async function () {
+    utils.loadRunConfig.mockImplementation(() => ({
+      ...baseRunCfg,
+      suites: [
+        {
+          ...baseSuite,
+        }
+      ]
+    }));
+    process.env = {
+      SAUCE_VM: 'truth',
+    };
+    runReturnValue = 1;
+    const passed = await run('/fake/path/to/runCfg', 'fake-suite-name');
+    expect(passed).toBe(false);
+  });
+});
