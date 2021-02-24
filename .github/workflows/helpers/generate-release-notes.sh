b8ad2321627fa76f390e09d572349ffa8e1d662e
Félix P <felix@saucelabs.com>
2021-02-24 15:12:41 -0800

    Support multi-releases

--- .github/workflows/helpers/generate-release-notes.sh
+++ .github/workflows/helpers/generate-release-notes.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-## Add Changelog
-echo "## Changelog"
-git --no-pager log --no-notes --no-decorate --oneline  v${1}...master
+## Gather Changelog
+CHANGELOG=$(git --no-pager log --no-notes --no-decorate --oneline  v${1}...HEAD)
 
-## Add Framework version
+## Gather Framework version
 TESTCAFE_VER=$(< package-lock.json jq -r '.dependencies["testcafe"].version')
 NODEJS_VER=$(grep NODE_VERSION= Dockerfile | cut -d '=' -f 2)
 
-echo ""
-echo "## Frameworks"
-echo "- TestCafe ${TESTCAFE_VER}"
-echo "- NodeJS ${NODEJS_VER}"
-
-
-## Add Browser versions
+## Gather Browser versions
 BASE_IMAGE=$(grep FROM Dockerfile | cut -d ' ' -f 2)
 docker pull "${BASE_IMAGE}" > /dev/null 2>&1 || exit 1
 FF_VER=$(docker inspect ${BASE_IMAGE} | jq -r '.[0].ContainerConfig.Env | .[] | select(. | startswith("FF_VER="))' | cut -d '=' -f 2)
 CHROME_VER=$(docker inspect ${BASE_IMAGE} | jq -r '.[0].ContainerConfig.Env | .[] | select(. | startswith("CHROME_VER="))' | cut -d '=' -f 2)
 
-echo ""
-echo "## Browsers"
-echo "- Firefox ${FF_VER}"
-echo "- Chrome ${CHROME_VER}";
+## Generate everything
+cat <<EOF
+
+## Changelog
+${CHANGELOG}
+
+## Frameworks
+- TestCafe ${TESTCAFE_VER}
+- NodeJS ${NODEJS_VER}
+
+## Browsers
+- Firefox ${FF_VER}
+- Chrome ${CHROME_VER}
+
+### Build Info
+<details>
+
+- jobId: ${GITHUB_RUN_ID}
+- branch: ${GITHUB_REF}
+
+</details>
+EOF
