e9c6685a1c2632d405a6288eb185c9130becce39
Tian Feng <tian.feng@saucelabs.com>
2021-07-13 16:13:55 -0700

    [DEVX-1000] Bump up testcafe version to v1.15.0

--- .github/helpers/update-saucectl-template.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## Add Framework version
-TESTCAFE_VER=$(< package-lock.json jq -r '.dependencies["testcafe"].version')
-
-sed -E -i "s/^  version: (.*)$/  version: ${TESTCAFE_VER}/g" .saucetpl/.sauce/config.yml
-git add .saucetpl/.sauce/config.yml
