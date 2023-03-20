40930c17a41694e66f34dfc3d3560d762d9cc9d2
Felix P <felix@saucelabs.com>
2023-03-20 17:47:00 +0100

    Target indedx.html

--- tests/cloud/tests/sc/sauce-connect.test.js
+++ tests/cloud/tests/sc/sauce-connect.test.js
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe';
 
-fixture `Sauce-Connect`.page `http://localhost:8000/`;
+fixture `Sauce-Connect`.page `http://localhost:8000/index.html`;
 
 test('Checking sauce-connect connectivity', async function (t) {
   await t.expect(Selector('title').innerText).eql('Simple Page');
