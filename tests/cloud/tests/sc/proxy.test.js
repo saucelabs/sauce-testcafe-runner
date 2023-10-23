b7ffd4a293650c9ac042d2b195aa522e53349c1e
Tian Feng <tian.feng@saucelabs.com>
2023-10-23 11:19:58 -0700

    update e2e tests for sauce-connect and proxy

--- tests/cloud/tests/sc/proxy.test.js
+++
@@ -1,8 +0,0 @@
-import { Selector, fixture, test } from 'testcafe';
-
-fixture `Sauce-Connect-Proxy`.page `https://artifactory.tools.saucelabs.net/ui/packages`;
-
-test('Checking sauce-connect connectivity', async function(t) {
-  await t.expect(Selector('title').innerText).eql('JFrog');
-});
-
