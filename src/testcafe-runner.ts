80c63e2220180898a91ad1a56c999b7f81bd66a5
Tian Feng <tian.feng@saucelabs.com>
2023-10-23 09:32:21 -0700

    add comments to explain why we need the os proxy

--- src/testcafe-runner.ts
+++ src/testcafe-runner.ts
@@ -275,6 +275,11 @@ async function run(nodeBin: string, runCfgPath: string, suiteName: string) {
     suite
   } = await prepareConfiguration(nodeBin, runCfgPath, suiteName);
 
+  // TestCafe used a reverse proxy for browser automation before.
+  // With TestCafe 3.0.0 and later, native automation was adopted,
+  // introducing CDP support for Chrome.
+  // This results HTTP requests can't be routed through the proxy.
+  // Now, we need to set up an OS-level proxy connection.
   if (suite.browserName === 'chrome' && !isCDPDisabled() && isProxyAvailable()) {
     setupProxy();
   }
