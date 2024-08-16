5e0fbfe0cb58bba197f200627e6a1b783220809b
Tian Feng <tian.feng@saucelabs.com>
2024-08-16 15:12:00 -0700

    chore: Remove redundant console.log

--- src/runner.ts
+++ src/runner.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import fs from 'fs';
 import path from 'path';
-import * as stream from 'stream';
 import child_process from 'child_process';
 import * as utils from 'sauce-testrunner-utils';
 import { TestCafeConfig } from './type';
@@ -20,12 +19,6 @@ async function testCafeRunner() {
     if (!fs.existsSync(assetsPath)) {
       fs.mkdirSync(assetsPath);
     }
-    const fd = fs.openSync(path.join(assetsPath, 'console.log'), 'w+', 0o644);
-    const ws = new stream.Writable({
-      write(data: any, encoding: any, cb: any) {
-        fs.write(fd, data, undefined, encoding, cb);
-      },
-    });
 
     const [nodeBin] = process.argv;
     const testcafeRunnerEntry = path.join(__dirname, 'testcafe-runner.js');
@@ -36,11 +29,8 @@ async function testCafeRunner() {
 
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
-    child.stdout.pipe(ws);
-    child.stderr.pipe(ws);
 
     child.on('exit', (exitCode: number) => {
-      fs.closeSync(fd);
       if (exitCode === 0) {
         resolve();
       } else {
