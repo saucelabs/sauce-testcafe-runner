3d42d62fbe10b502f91bf50ff425d3a7abb6c075
Mike Han <mike.han@saucelabs.com>
2024-10-17 14:52:18 -0600

    chore: Update jest config to team standards

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,7 @@ const config = {
   testMatch: ['**/tests/unit/**/*.[jt]s?(x)'],
   collectCoverageFrom: ['src/**/*.ts'],
   collectCoverage: true,
-  preset: 'ts-jest',
-  transformIgnorePatterns: [
-    '/node_modules/(?!(axios)/)', // Uses module import statements, which aren't supported by jest, so it has to be transformed by babel.
-  ],
+  transform: { '^.+\\.ts?$': 'ts-jest' },
 };
 
 module.exports = config;
