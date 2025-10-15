e9e508dfeff75a8e615f5ea4a20b86bf2385a614
Ben Leavitt <ben.leavitt@saucelabs.com>
2025-10-15 13:25:52 -0600

    fix dep

--- src/testcafe-runner.ts
+++ src/testcafe-runner.ts
@@ -1,5 +1,5 @@
 //import { spawn, spawnSync } from 'child_process';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 import path from 'path';
 import fs from 'fs';
 import { URL } from 'node:url';
