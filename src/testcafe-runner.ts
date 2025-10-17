70b8b2e28f59678513eb07e5e96e1d7fd7cb5ca6
Ben Leavitt <ben.leavitt@saucelabs.com>
2025-10-17 17:09:50 -0600

    npm cli

--- src/testcafe-runner.ts
+++ src/testcafe-runner.ts
@@ -84,6 +84,8 @@ async function prepareConfiguration(
   // Define node/npm path for execution
   const npmBin = path.join(
     path.dirname(nodeBin),
+    '..',
+    'lib',
     'node_modules',
     'npm',
     'bin',
